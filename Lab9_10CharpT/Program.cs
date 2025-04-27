66b944f865954adbea9d925d24d2ba4d4eff038e
Ilona Chupa <159003642+ChupaIlona@users.noreply.github.com>
2025-04-27 17:13:43 +0300

    Update Program.cs
    Лабораторна робота №10

--- Lab9_10CharpT/Program.cs
+++ Lab9_10CharpT/Program.cs
@@ -1,8 +1,119 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Lab#9  or  Lab#10");
-//  За бажанням студента для задач можна створювати консольний проект або WinForm
-// Бажано для задач лаб. робіт створити окремі класи
-// Виконання  виконати в стилі багатозаданості :
-//   Lab9T2  lab9task2 = new Lab9T2; lab9task2.Run();
-// При бажанні можна створити багатозадачний режим виконання задач.
+using System;
+
+
+namespace lab10
+{
+    internal class Program
+    {
+        public class Part
+        {
+            public int Id { get; }
+            public string Name { get; }
+            public string Status { get; set; }
+
+            public Part(int id, string name)
+            {
+                Id = id;
+                Name = name;
+                Status = "Створено";
+            }
+        }
+
+        public class Station
+        {
+            public string Name { get; }
+            public event EventHandler<Part> ProcessingCompleted;
+
+            public Station(string name)
+            {
+                Name = name;
+            }
+
+            // Метод обробки деталі
+            public void Process(Part part)
+            {
+                Console.WriteLine($"{Name}: Почалася обробка деталі {part.Name}");
+                Thread.Sleep(1000); 
+                part.Status = $"Оброблено на {Name}";
+                Console.WriteLine($"{Name}: Завершила обробку деталі {part.Name}");
+
+                
+                if (ProcessingCompleted != null)
+                {
+                    ProcessingCompleted(this, part);
+                }
+            }
+        }
+
+        public class Conveyor
+        {
+            private List<Station> _stations;
+            private int _currentStationIndex;
+
+            public Conveyor(List<Station> stations)
+            {
+                _stations = stations;
+            }
+
+            public void Start(Part part)
+            {
+                _currentStationIndex = 0;
+                SubscribeToStations();
+                _stations[_currentStationIndex].Process(part);
+            }
+
+            // Підписка на події станцій
+            private void SubscribeToStations()
+            {
+                foreach (var station in _stations)
+                {
+                    station.ProcessingCompleted += (sender, part) =>
+                    {
+                        _currentStationIndex++;
+                        if (_currentStationIndex < _stations.Count)
+                        {
+                            _stations[_currentStationIndex].Process(part);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Деталь {part.Name} завершила всі етапи обробки!");
+                        }
+                    };
+                }
+            }
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            var stations = new List<Station>
+            {
+                new Station("Різання"),
+                new Station("Шліфування"),
+                new Station("Фарбування")
+            };
+            var conveyor = new Conveyor(stations);
+
+           // Console.WriteLine("Вкажіть кількість деталей для обробки:");
+            //int number_of_parts = int.Parse(Console.ReadLine());
+
+
+           // for(int i = 0; i < number_of_parts; i++)
+           // {
+           //     conveyor.Start(new Part(i, "00" + i));
+           // }
+           var part = new Part(1, "Деталь 001");
+
+            
+            conveyor.Start(part);
+
+            Console.WriteLine("Натисніть будь-яку клавішу для завершення...");
+            Console.ReadKey();
+
+        }
+    }
+}
+
 
